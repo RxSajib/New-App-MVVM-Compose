13049fb7e205e3e39c8a878d5ee6bcc76713201c
RxSajib <sajibroy206@gmail.com>
2025-02-17 10:22:28 +0600

    add details appbar

---
+++ app/src/main/java/com/news/newsappmvvm/presentation/onboardingscreen/presentation/details/component/DetailsTopBar.kt
@@ -0,0 +1,66 @@
+package com.news.newsappmvvm.presentation.onboardingscreen.presentation.details.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.news.newsappmvvm.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailsTopBar(
+    onBrowsingClick: () -> Unit,
+    onShareClick: () -> Unit,
+    onBookMarkClick: () -> Unit,
+    onBackPress: () -> Unit
+) {
+    TopAppBar(
+        title = {},
+        modifier = Modifier.fillMaxWidth(),
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = Color.Transparent,
+            actionIconContentColor = colorResource(id = R.color.black),
+            navigationIconContentColor = colorResource(id = R.color.black)
+        ),
+        navigationIcon = {
+            IconButton(onClick = onBackPress) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = onBookMarkClick) {
+                Icon(painter = painterResource(id = R.drawable.bookmark_svgrepo_com), contentDescription = null)
+            }
+            IconButton(onClick = onShareClick) {
+                Icon(painter = painterResource(R.drawable.search_alt_svgrepo_com), contentDescription = null)
+            }
+            IconButton(onClick = onBrowsingClick) {
+                Icon(painter = painterResource(R.drawable.network_backup_svgrepo_com), contentDescription = null)
+            }
+        }
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun Preview() {
+    DetailsTopBar(
+        onBrowsingClick = {},
+        onShareClick = {},
+        onBookMarkClick = {},
+        onBackPress = {}
+    )
+}
--- app/src/main/java/com/news/newsappmvvm/presentation/onboardingscreen/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/news/newsappmvvm/presentation/onboardingscreen/presentation/search/SearchViewModel.kt
@@ -1,5 +1,6 @@
 package com.news.newsappmvvm.presentation.onboardingscreen.presentation.search
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.news.newsappmvvm.presentation.onboardingscreen.component.domain.usercase.news.NewsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -9,4 +10,26 @@ import javax.inject.Inject
 class SearchViewModel @Inject constructor(
     private val newsUseCase: NewsUseCase
 ) : ViewModel() {
+
+    private val _state = mutableStateOf(SearchState())
+
+    fun onEvent(event: SearchEvent) {
+        when (event) {
+            is SearchEvent.updateSearchQuery -> {
+                _state.value = _state.value.copy(searchQuery = event.searchQuery)
+            }
+
+            is SearchEvent.searchNews -> {
+                searchName()
+            }
+        }
+
+    }
+
+    private fun searchName(){
+        /*val articles = newsUseCase.searchNews(
+
+        )*/
+    }
+
 }
